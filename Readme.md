83b1ae64b121e9ccf3c7647e4bc1358c5381540d
Tanver Hasan <t.h.noman@outlook.com>
2019-10-18 22:19:28 +0100

    Added Readme.md and removed git cache

---
+++ Readme.md
@@ -0,0 +1,18 @@
+### GraphQL API
+
+#### Install Node packages listed in package.json
+``` 
+npm install
+```
+#### Build the project
+```
+npm run build
+```
+#### Run the project 
+
+```
+npm run start
+
+```
+
+
--- dist/schema.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var graphql_1 = require("graphql");
-var RootQueryType_1 = require("./RootQueryType");
-exports.Schema = new graphql_1.GraphQLSchema({
-    query: RootQueryType_1.RootQueryType,
-});
