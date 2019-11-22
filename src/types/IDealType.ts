a9ac2cff5ec6a88a1fe1993170cdad50be18c59d
Will Sheehan <willsheehan95@gmail.com>
2019-11-22 14:47:50 -0500

    graphql types

--- src/types/IDealType.ts
+++ src/types/IDealType.ts
@@ -1,4 +1,4 @@
-import { GraphQLID, GraphQLInt, GraphQLList, GraphQLObjectType, GraphQLString } from "graphql";
+import { GraphQLID, GraphQLInt, GraphQLList, GraphQLObjectType, GraphQLString, GraphQLBoolean } from "graphql";
 
 const IdealInvestorType = new GraphQLObjectType({
     name: "IdealInvestorType",
@@ -14,26 +14,8 @@ export const IDealType = new GraphQLObjectType({
         company_description: { type: GraphQLString },
         investment_documents: { type: GraphQLString },
         date_closed: { type: GraphQLString },
-        entity_name: { type: GraphQLString },
-        deal_name: { type: GraphQLString },
-        amount_wired: { type: GraphQLInt },
-        amount: { type: GraphQLString },
-        name: { type: GraphQLString },
-        user_id: { type: GraphQLString },
-        total_investors: { type: GraphQLInt },
-        deal_complete_date: { type: GraphQLString },
-        operations_agreement: { type: GraphQLString },
-        subscription_agreement: { type: GraphQLString },
-        private_placement_memorandum: { type: GraphQLString },
-        createdAt: { type: GraphQLString },
-        updatedAt: { type: GraphQLString },
-        investors: { type: new GraphQLList(IdealInvestorType) },
-        bank_account: { type: GraphQLString},
-        formation_certificate_filing: { type: GraphQLString},
-        ein_filing: { type: GraphQLString},
-        form_d_filing: { type: GraphQLString},
-        form_1065_filing: {type: GraphQLString},
-        w9_filing: {type: GraphQLString},
+        closed: { type: GraphQLBoolean },
+        amount: { type: GraphQLInt }
     },
 });
 export const IDealDeleteType = new GraphQLObjectType({
