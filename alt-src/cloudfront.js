ff9fa04d3578368c01c19a4b44026f85a6d57444
Will Sheehan <willsheehan95@gmail.com>
2019-12-06 10:21:41 -0500

    read priv key from s3 in cloudfront signing

--- alt-src/cloudfront.js
+++ alt-src/cloudfront.js
@@ -1,22 +1,28 @@
+const S3 = require('aws-sdk/clients/s3')
 const cloudfrontSign = require('aws-cloudfront-sign')
 
 const { CLOUDFRONT_URL, CLOUDFRONT_PUBLIC_KEY, CLOUDFRONT_PRIVATE_KEY } = process.env
 
-const params = {
-  keypairId: CLOUDFRONT_PUBLIC_KEY,
-  privateKeyString: CLOUDFRONT_PRIVATE_KEY
-}
+const s3 = new S3({apiVersion: '2006-03-01'})
 
 const hour = 60 * 60 * 1000
 function expiry () {
   return Date.now() + (4 * hour)
 }
 
-function getSignedUrl (path) {
+const cachedPrivateKey = null
+async function getSignedUrl (path) {
+  const privateKeyString = cachedPrivateKey || await getPrivKey() 
+
   return cloudfrontSign.getSignedUrl(
     CLOUDFRONT_URL + "/" + path,
-    {...params, expireTime: expiry()}
+    {privateKeyString, keypairId: CLOUDFRONT_PUBLIC_KEY, expireTime: expiry()}
   )
+}
+
+async function getPrivKey () {
+  const res = await s3.getObject({ Bucket: 'allocations-credentials', Key: "cloudfront-private-key.pem"}).promise()
+  return res.Body.toString()
 }
 
 module.exports = { getSignedUrl }
--- test/cloudfront.test.js
+++ test/cloudfront.test.js
@@ -1,7 +1,7 @@
 require('dotenv').config()
 const { getSignedUrl } = require('../alt-src/cloudfront')
 
-test("cloudfront signs correctly", () => {
-  const url = getSignedUrl("/5de7df4441d111dcc8de83ef/test.pdf")
+test("cloudfront signs correctly", async () => {
+  const url = await getSignedUrl("/5de7df4441d111dcc8de83ef/test.pdf")
   expect(url)
 })
