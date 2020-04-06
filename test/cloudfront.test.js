5cad928fb074242ce810332205d268138b9ec92b
Will Sheehan <willsheehan95@gmail.com>
2020-04-06 12:34:06 -0400

    fix path in cloudfront test

--- test/cloudfront.test.js
+++ test/cloudfront.test.js
@@ -1,5 +1,5 @@
 require('dotenv').config()
-const { getSignedUrl } = require('../alt-src/cloudfront')
+const { getSignedUrl } = require('../src/cloudfront')
 
 test("cloudfront signs correctly", async () => {
   const url = await getSignedUrl("/5de7df4441d111dcc8de83ef/test.pdf")
