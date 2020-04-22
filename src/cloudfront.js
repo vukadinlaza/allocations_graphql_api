8a505b2f0ce9b6444793e7732ebff0bff62d839e
Will Sheehan <willsheehan95@gmail.com>
2020-04-22 10:23:45 -0400

    use encrypted bucket url to maintain backwards compatibility

--- src/cloudfront.js
+++ src/cloudfront.js
@@ -1,7 +1,7 @@
 const S3 = require('aws-sdk/clients/s3')
 const cloudfrontSign = require('aws-cloudfront-sign')
 
-const { CLOUDFRONT_URL, CLOUDFRONT_PUBLIC_KEY, CLOUDFRONT_PRIVATE_KEY } = process.env
+const { CLOUDFRONT_URL, CLOUDFRONT_ENCRYPTED_URL, CLOUDFRONT_PUBLIC_KEY, CLOUDFRONT_PRIVATE_KEY } = process.env
 
 const s3 = new S3({apiVersion: '2006-03-01'})
 
@@ -12,17 +12,22 @@ function expiry () {
 
 const cachedPrivateKey = null
 async function getSignedUrl (path) {
-  const privateKeyString = cachedPrivateKey || await getPrivKey() 
+  const privateKeyString = cachedPrivateKey || await getPrivKey()
+
+  // currently there are 2 cloudfronts, 1 for encrypted one for legacy w/ diff paths
+  const baseUrl = path.slice(0, 12) === "investments/" ? CLOUDFRONT_ENCRYPTED_URL : CLOUDFRONT_URL 
 
   return cloudfrontSign.getSignedUrl(
-    CLOUDFRONT_URL + "/" + path,
+    baseURL + "/" + path,
     {privateKeyString, keypairId: CLOUDFRONT_PUBLIC_KEY, expireTime: expiry()}
   )
 }
 
 async function getPrivKey () {
   const res = await s3.getObject({ Bucket: 'allocations-credentials', Key: "cloudfront-private-key.pem"}).promise()
-  return res.Body.toString()
+  const key = res.Body.toString()
+  cachedPrivateKey = key
+  return key
 }
 
 module.exports = { getSignedUrl }
