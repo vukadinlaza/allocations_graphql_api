e823796c4a9e43f0788b3c5da1d38925ab0a7d71
Lance Owen Merrill <lanceomerrill@gmail.com>
2020-07-22 08:02:01 -0700

    remove console.log

--- src/utils/docusign.js
+++ src/utils/docusign.js
@@ -167,8 +167,6 @@ const getKYCTemplateId = async ({input, accountId}) => {
     const isUsCitizen = input.country === 'United States'
     const templates = await templatesApi.listTemplates(accountId)
 
-    console.log(templates, input)
-
     const kycDocuments = [	
     {	
     isUsCitizen: true,	
