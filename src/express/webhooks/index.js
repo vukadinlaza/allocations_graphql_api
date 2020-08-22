e2c7db163f12f99ead896d67be7583a1b4d0e7be
Lance Merrill <lanceomerrill@gmail.com>
2020-08-22 08:34:15 -0700

    more testing

--- src/express/webhooks/index.js
+++ src/express/webhooks/index.js
@@ -53,7 +53,10 @@ module.exports = Router()
           deal_id: ObjectId(dealId),
           user_id: ObjectId(user._id),
         })
+        console.log(get(docusignData, 'DocuSignEnvelopeInformation.DocumentPDFs.DocumentPDF')
+        )
         const pdf = get(docusignData, 'DocuSignEnvelopeInformation.DocumentPDFs.DocumentPDF.PDFBytes._text')
+        console.log('PDF', pdf)
         let buff = new Buffer(pdf, 'base64');
         let text = buff.toString('ascii');
 
