43bcc74daaae0179f30337c1b4cb9a30c11a69d9
Lance Merrill <lanceomerrill@gmail.com>
2020-09-14 09:13:07 -0700

    allow funds of the same name

--- src/graphql/resolvers/deals.js
+++ src/graphql/resolvers/deals.js
@@ -125,7 +125,7 @@ const Mutations = {
     const slug = _.kebabCase(deal.company_name)
 
     // ensure that deal name with org doesn't exist
-    const collision = await ctx.db.deals.findOne({ slug })
+    const collision = await ctx.db.deals.findOne({ slug, organization: org.id })
     if (collision) {
       throw new Error("Deal with same name already exists")
     }
