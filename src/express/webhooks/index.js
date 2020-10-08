90cb16666111acc7e804aa8e9712c1de4665ab05
Lance Merrill <lanceomerrill@gmail.com>
2020-10-08 10:51:40 -0700

    remove console logs

--- src/express/webhooks/index.js
+++ src/express/webhooks/index.js
@@ -43,7 +43,6 @@ module.exports = Router()
       const userEmail = get(emailfield, 'value._text')
 
       let user = await db.users.findOne({ email: signerEmail.toLowerCase() });
-      console.log('fires 1', user)
       if (!user) {
         if (userEmail) {
           user = await db.users.findOne({ email: userEmail });
@@ -86,8 +85,6 @@ module.exports = Router()
           ContentEncoding: 'base64',
           ContentType: "application/pdf",
         }
-        console.log('fires 2', investment)
-        console.log('fires 3', key)
 
         const s3Res = await s3.upload(obj).promise()
 
