7365efff690983cce156c0f049dac145281960ce
Lance Merrill <lanceomerrill@gmail.com>
2020-10-20 13:56:36 -0700

    working

--- src/express/webhooks/index.js
+++ src/express/webhooks/index.js
@@ -166,7 +166,7 @@ module.exports = Router()
       next(err);
     }
   })
-  .post('/slack', async (req, res, next) => {
+  .post('/slac', async (req, res, next) => {
     try {
       console.log(req.body)
       const { challenge } = req.body
