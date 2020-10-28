ddbbc3e7a63570e6245404cb1b72307aec6840f1
Lance Merrill <lanceomerrill@gmail.com>
2020-10-28 11:28:03 -0700

    add investparam

--- src/graphql/schema/users.js
+++ src/graphql/schema/users.js
@@ -34,7 +34,8 @@ type User {
   mail_city: String
   mail_zip: String
   mail_state: String
-  mail_street_address: String
+  mail_street_address: String,
+  showInvestAndMrkPlc: Boolean
 }
 
 input UserInput {
@@ -50,6 +51,7 @@ input UserInput {
   passport: Upload
   accredidation_doc: Upload
   terms_of_service: Boolean
+  showInvestAndMrkPlc: Boolean
 }
 
 extend type Query {
