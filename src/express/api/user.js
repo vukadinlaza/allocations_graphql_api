1d47b794e915fd6024f8729eddd6733f3c42a709
Lance Merrill <lanceomerrill@gmail.com>
2020-11-04 11:13:09 -0800

    many welps

--- src/express/api/user.js
+++ src/express/api/user.js
@@ -2,18 +2,19 @@ const { Router } = require('express');
 const { get, pick } = require('lodash')
 const { connect } = require('../../mongo/index')
 
-const apiKeys = ['5fa2d72131ed7b7bc4666fe5']
+const apiKeys = [{ key: '5fa2d72131ed7b7bc4666fe5', source: 'TVC' }]
 module.exports = Router()
 	.post('/:API_KEY', async (req, res, next) => {
 
 		try {
-			if (!apiKeys.includes(req.params.API_KEY)) {
+			const key = apiKeys.find(k => k.key === req.params.API_KEY)
+			if (!key) {
 				return res.status(400).send({
 					status: 400,
 					error: 'Invalid API key'
 				})
 			}
-			const payload = pick(req.body, ['email', 'first_name', 'last_name', 'investor_type', 'entity_name'])
+			const payload = pick(req.body, ['email', 'first_name', 'last_name', 'investor_type', 'entity_name',])
 
 			if (!req.body.email) {
 				return res.status(400).send({
@@ -26,10 +27,10 @@ module.exports = Router()
 			if (user) {
 				return res.status(400).send({
 					status: 400,
-					error: 'user already exists : { email: "test@test.com" }'
+					error: 'user already exists'
 				})
 			}
-			const createdUser = await db.users.insertOne({ ...payload, created_at: Date.now() })
+			const createdUser = await db.users.insertOne({ ...payload, source: key.source, created_at: Date.now() })
 			createdUser.ops[0]
 
 			return res.status(200).send(createdUser.ops[0])
