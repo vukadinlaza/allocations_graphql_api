927d267bff41657bfbb43368a8830cbf90ca0bce
Adrian <adri.suscripciones@gmail.com>
2021-07-08 18:58:11 -0400

    websockets setup

--- src/graphql/server.js
+++ src/graphql/server.js
@@ -1,26 +1,28 @@
 const { ApolloServer, PubSub } = require('apollo-server-express')
 const { verify, authenticate } = require('../auth')
-
 const logger = require('../utils/logger')
 const { typeDefs, resolvers } = require('../graphql/resolvers')
 
 const pubsub = new PubSub();
 
-function authedServer (db) {
+function authedServer(db) {
   const publicEndpoints = ["PublicDeal"]
 
-  return new ApolloServer({ 
+  return new ApolloServer({
     typeDefs,
     resolvers,
-    context: async ( payload ) => {
-      // public deal endpoint skips authentication   
-      console.log({payload})
-      if (payload.req && payload.req.body && publicEndpoints.includes(req.body.operationName)) {
+    context: async (payload) => {
+      // public deal endpoint skips authentication  
+      console.log({payload}) 
+      // if (payload && payload.connection && payload.connection.query.includes('subscription')) {
+      //   return { db, pubsub }
+      // }
+      if (payload.req && payload.req.body && publicEndpoints.includes(payload.req.body.operationName)) {
         return { db }
       }
-
-      const user = await authenticate({ req: payload.req, db })
-
+      console.log('FINALLY', payload.connection.context)
+      const authToken = payload && payload.connection && payload.connection.context && payload.connection.context.authToken
+      const user = await authenticate({ req: payload.req, db, authToken })
       return { user, db, pubsub }
     },
     plugins: [
@@ -37,5 +39,4 @@ function authedServer (db) {
     ]
   })
 }
-
 module.exports = { authedServer, pubsub }
