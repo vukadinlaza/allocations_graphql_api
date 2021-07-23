413a3b3b46bd76c8302f3891387db3a237c718f1
BRYANA <BRYANAKITCHEN@GMAIL.COM>
2021-07-23 08:45:39 -0400

    format all deal AUM amounts to be numbers for accurate uniformity

--- src/graphql/resolvers/deals.js
+++ src/graphql/resolvers/deals.js
@@ -98,12 +98,13 @@ const Deal = {
   },
   dealOnboarding: async (deal, _, { db }) => {
     const dealOnboarding = await db.dealOnboarding.findOne({ dealName: deal.company_name })
+
     return dealOnboarding
   },
   AUM: async (deal, _, { db }) => {
-    const wiredInvestments = await db.investments.find({deal_id: deal._id, status: {$in: ['wired', 'complete']}}).toArray();
-    console.log(JSON.stringify(wiredInvestments))
-    const aum = wiredInvestments.length? wiredInvestments.map(inv => inv.amount).reduce((acc, n) => acc + n) : 0
+    const wiredInvestments = await db.investments.find({ deal_id: deal._id, status: { $in: ['wired', 'complete'] } }).toArray();
+
+    const aum = wiredInvestments.length ? wiredInvestments.map(inv => inv.amount).reduce((acc, n) => Number(acc) + Number(n)) : 0
     return aum
   }
 }
@@ -161,10 +162,10 @@ const Queries = {
     const aggregation = getPagAggregation(args.pagination, additionalFilter)
 
     let query = ctx.db.collection("deals")
-                      .aggregate(aggregation)
-                      .skip(documentsToSkip)
-                      .limit(pagination)
-                      .toArray()
+      .aggregate(aggregation)
+      .skip(documentsToSkip)
+      .limit(pagination)
+      .toArray()
     return query;
   }
 }
@@ -210,7 +211,7 @@ const Mutations = {
   /** special handling for wire instructions upload **/
   updateDeal: async (_, { org, deal: { _id, wireDoc, ...deal } }, ctx) => {
     const { user } = ctx;
-    console.log('HEREEE')
+
     ctx.pubsub.publish('dealOnboarding', { dealOnboarding: 'YES' })
     if (deal.isPostingComment) {
       const res = await ctx.db.deals.findOneAndUpdate(
@@ -470,9 +471,8 @@ const Mutations = {
     )
   },
   addUserAsViewed: async (_, { user_id, deal_id }, ctx) => {
-    console.log('USER DEAL', user_id, deal_id)
     const deal = await ctx.db.deals.findOne({ _id: ObjectId(deal_id) })
-    console.log(deal.usersViewed)
+
     if ((deal.usersViewed || []).map(i => String(i)).find(id => id === user_id)) {
       return deal
     }
