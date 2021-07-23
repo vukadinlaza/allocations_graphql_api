cf2ba2a75af3773c2492071203f0f8de0cd01619
BRYANA <BRYANAKITCHEN@GMAIL.COM>
2021-07-23 17:54:09 -0400

    merged conflicts from AM

--- src/graphql/helpers.js
+++ src/graphql/helpers.js
@@ -33,27 +33,6 @@ module.exports = {
       }
     })
 
-    let sortBy = {};
-    sortBy[`${sortNestedKey ? `${sortField}.${sortNestedKey}` : (sortField ? sortField : filterField)}`] = (sortOrder ? sortOrder : 1)
-
-    let aggregation = []
-    if (sortNestedKey && sortNestedCollection && sortLocalFieldKey) aggregation.push({
-      $lookup: {
-        from: sortNestedCollection,
-        localField: sortLocalFieldKey,
-        foreignField: '_id',
-        as: sortField
-      }
-    })
-    if (filterNestedKey && filterNestedCollection && filterLocalFieldKey) aggregation.push({
-      $lookup: {
-        from: filterNestedCollection,
-        localField: filterLocalFieldKey,
-        foreignField: '_id',
-        as: filterField
-      }
-    })
-
     aggregation.push({ $match: match })
     if (sortField && sortOrder) aggregation.push({ $sort: sortBy })
 
