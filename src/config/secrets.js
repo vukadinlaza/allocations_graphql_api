2e473f961aa4f2fd263fb21c28aa0cdaa3f3cd10
CapyJara <42257433+CapyJara@users.noreply.github.com>
2021-12-28 08:31:48 -0800

    hive 3079 - aws secret manager (#495)
    * in progress
    
    * remove log
    
    * remove from docusign
    
    * call in getawssecrets in app
    
    * created server to init secretes before app runs

---
+++ src/config/secrets.js
@@ -0,0 +1,22 @@
+require("dotenv").config();
+const { SecretsManager } = require("aws-sdk");
+
+const secretsManager = new SecretsManager({ region: "us-east-1" });
+
+const AWS_ENV_SECRET_NAME = "allocation_graphql_api";
+
+const getAwsSecrets = async () => {
+  const { SecretString } = await secretsManager
+    .getSecretValue({
+      SecretId: `${AWS_ENV_SECRET_NAME}/${process.env.NODE_ENV}`,
+    })
+    .promise();
+
+  const secrets = JSON.parse(SecretString);
+
+  Object.entries(secrets).forEach(([key, envKey]) => {
+    process.env[key] = envKey;
+  });
+};
+
+module.exports = getAwsSecrets;
