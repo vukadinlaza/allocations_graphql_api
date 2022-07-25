47c4a0835dc1b2499afffa641954ecb6e87c9792
Chase Abbott <cabbott93@gmail.com>
2022-07-25 13:20:01 -0700

    whitelist

--- src/docspring/index.js
+++ src/docspring/index.js
@@ -117,6 +117,7 @@ const getTemplateData = (input, user, templateId) => {
     "tpl_RPkFTESPMpYPhMJ6eY",
     "tpl_JyxfDpAA3h6f2aqhxh",
     "tpl_qrZ9GtmZCNJZhG5NYX",
+    "tpl_fXGt9aAd5XAAcHcMFR",
   ];
 
   const techstarDeals = [
