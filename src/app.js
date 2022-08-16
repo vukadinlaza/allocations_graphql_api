2e4c94f1fbb8b00a988890e489d5db3a95f30da3
Zilula <lanceomerrill@gmail.com>
2022-08-16 08:55:48 -0700

    remove console log

--- src/app.js
+++ src/app.js
@@ -23,7 +23,6 @@ const { NODE_ENV } = process.env;
 
 function corsWhitelist(whitelist) {
   const origin = (origin, cb) => {
-    console.log("ORIGIN", origin);
     if (origin && origin.includes("vercel.app")) {
       return cb(null, true);
     }
